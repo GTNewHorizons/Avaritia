d2f94f61ec5b2b0c10997c37899b26ca69bfc467
__felix__ <134875831+felixfour@users.noreply.github.com>
2025-04-22 12:42:06 -0400

    update comment

--- src/main/java/fox/spiteful/avaritia/compat/Compat.java
+++ src/main/java/fox/spiteful/avaritia/compat/Compat.java
@@ -51,7 +51,9 @@ public class Compat {
         ae2 = Loader.isModLoaded("appliedenergistics2") && Config.ae2;
         exu = Loader.isModLoaded("ExtraUtilities") && Config.exu;
         ic2 = Loader.isModLoaded("IC2") && Config.ic2;
-        // NOTE: This will only work with gt5 if you also exclude gregapi. This doesn't cause errors with gt6 now, but it might if anyone plans on using it specifically as a *gt5* check
+        // NOTE: This will only work with gt5 unless you add ` && !Loader.isModLoaded("gregapi")`.
+        // This doesn't cause errors with gt6 now, but it might if anyone plans on using it specifically as a *gt5* check
+        // Also if you want a GTNH *specific* feature to have compat, use new modid gregtechNH.
         gt = Loader.isModLoaded("gregtech") && Config.gt;
         botan = Loader.isModLoaded("Botania") && Config.botan;
         blood = Loader.isModLoaded("AWWayofTime") && Config.blood;
